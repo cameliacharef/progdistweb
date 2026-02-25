1b3b2e0d439cf050707fe7344cf44905c5b8b4fa
CHAREF <Camelia.Charef@etu.sorbonne-universite.fr>
2026-02-25 17:02:45 +0100

    modif

--- room-booking-platform/frontend/src/config.js
+++ room-booking-platform/frontend/src/config.js
@@ -2,9 +2,10 @@
 const config = {
   // Pour le développement local avec docker-compose
   API_URLS: {
-    rooms: 'http://localhost:8080/api',
-    bookings: 'http://localhost:8000',
-    users: 'http://localhost:3000/api'
+    rooms: '/api/rooms',      // ✅ chemin relatif
+    bookings: '/api/bookings',
+    users: '/api/users'
+  
   }
   // Plus tard, vous pourrez changer ces URLs pour Kubernetes
   // en utilisant une variable d'environnement
